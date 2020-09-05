74f25d4660f33a6dee1047fbfaf9c3295f85aff7
Touhidul Shawan <touhidulshawan@gmail.com>
2020-09-05 20:36:47 +0600

    Updates

---
+++ precache-manifest.d376cb87d3e4249105c169f7d63ceaea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9bff79d46a5fa39fedeaacb78cdcfc",
+    "url": "/pop-quiz/index.html"
+  },
+  {
+    "revision": "ce5c79505204cc252409",
+    "url": "/pop-quiz/static/css/main.51ad9f5d.chunk.css"
+  },
+  {
+    "revision": "6334e7f8499ae7d8e1b6",
+    "url": "/pop-quiz/static/js/2.419e2e9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pop-quiz/static/js/2.419e2e9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5c79505204cc252409",
+    "url": "/pop-quiz/static/js/main.68500ef2.chunk.js"
+  },
+  {
+    "revision": "96461e89d195c15fd260",
+    "url": "/pop-quiz/static/js/runtime-main.3f530d6b.js"
+  }
+]);
