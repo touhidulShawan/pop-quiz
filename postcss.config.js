278ce6c744f141fce51e5d60c7a8f7393db9c1f5
Touhidul Shawan <touhidulshawan@gmail.com>
2020-09-05 20:24:04 +0600

    remove unnecessary css and configure to deploy

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("@fullhuman/postcss-purgecss")({
+      content: [
+        "./src/**/*.js",
+        "./src/**/*.jsx",
+        "./public/index.html",
+        "./src/**/*.css",
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+  ],
 };
